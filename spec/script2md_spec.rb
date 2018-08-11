cd1f99d08cff44da8e5f9f83c0685cf00fda4328
Yumainaura <yuma.inaura@gmail.com>
2018-08-11 18:39:07 +0900

    Fix Gemfile

--- spec/script2md_spec.rb
+++ spec/script2md_spec.rb
@@ -1,9 +1,32 @@
 RSpec.describe Script2md do
-  it "has a version number" do
-    expect(Script2md::VERSION).not_to be nil
-  end
+  describe '.convert' do
+    subject { described_class.convert(text)  } 
+    let(:text) do
+      <<~SCRIPT
+        #!/bin/bash -eu
+        
+        # # Header1
+        #
+        # This is a shell script comment
+        # will be converted to markdown plain text
 
-  it "does something useful" do
-    expect(false).to eq(true)
+        echo $((RANDOM))
+        echo ok # This line is 
+
+        $echo_command='echo convert to markdown'
+        eval $echo_command
+
+        # - list 1
+        # - list 2
+        # - list 3
+
+        echo can you find well converting
+        echo and formatting with this gem?
+      SCRIPT
+    end
+
+    it { is_expected.to include '# Header1' }
+    it { is_expected.not_to include '/bin/bash' }
+    it { is_expected.not_to include '\`\`\`bash' }
   end
 end
