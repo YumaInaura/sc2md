d0399b5e602434fc23e7369987f1bb50c8cdb402
Yumainaura <yuma.inaura@gmail.com>
2018-08-11 18:10:52 +0900

    Add Script2md.convert

--- lib/script2md.rb
+++ lib/script2md.rb
@@ -1,5 +1,12 @@
 require "script2md/version"
 
 module Script2md
-  # Your code goes here...
+  def self.convert(text)
+    output = text
+               .gsub(/^#!.+/, '')
+               .gsub(/(^(?!#)[^\n\r].+[\n\r]?)(^(?!#)[^\n\r]*[\n\r]?)*/, "```bash\n\\0\n```\n")
+               .gsub(/^#\s?([^\n\r]+)$/, "\\1\n")
+     
+     output
+  end
 end
