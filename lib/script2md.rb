cf44e190a5fde42f6f255d6289f4033e7d43dd9e
Yumainaura <yuma.inaura@gmail.com>
2018-08-11 23:24:04 +0900

    Fix converted markdown formatting

--- lib/script2md.rb
+++ lib/script2md.rb
@@ -2,11 +2,45 @@ require "script2md/version"
 
 module Script2md
   def self.convert(text)
-    output = text
-               .gsub(/^#!.+/, '')
-               .gsub(/(^(?!#)[^\n\r].+[\n\r]?)(^(?!#)[^\n\r]*[\n\r]?)*/, "```bash\n\\0\n```\n")
-               .gsub(/^#\s?([^\n\r]+)$/, "\\1\n")
-     
+    output = Convert.new(text).convert.text
+    output = output
      output
+  end
+
+  class Convert
+    def initialize(text)
+      @text = text
+    end
+
+    attr_reader :text
+
+    def convert
+      remove_shebang!
+      code_into_codeblock!
+      headling_comment_to_plaintext!
+      format!
+      self
+    end
+
+    private
+
+    attr_writer :text
+
+    def remove_shebang!
+      text.gsub!(/^#!.+/, '') 
+    end
+
+    def code_into_codeblock!
+      text.gsub!(/((^(?!#)[^\n\r].+[\n\r]?)(^(?!#).[^\n\r]*[\n\r]?)*)/, "```bash\n\\1```\n")
+    end
+
+    def headling_comment_to_plaintext!
+      text.gsub!(/^#\s+?$/, "")
+      text.gsub!(/^#\s+?([^\n\r]+)$/, "\\1")
+    end
+
+    def format!
+      text.gsub!(/\A[\n\r]+|[\n\r]+\z/, '') # Remove trailing empty lines
+    end
   end
 end
---
+++ spec/fixtures/example.md
@@ -0,0 +1,19 @@
+# Header1
+
+This is example bash script for
+convert to markdown style
+
+## Header2
+
+```bash
+echo ok
+echo ok # line end code comment
+```
+
+This is comment line
+This is comment line
+
+```bash
+echo some command
+echo "i wanna conver to markdown this file"
+```
