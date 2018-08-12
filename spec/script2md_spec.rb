9fcb6007aab4a3ccc99b191f23a2eeb2be22d9ce
Yumainaura <yuma.inaura@gmail.com>
2018-08-12 10:47:42 +0900

    Fix spec remove unneeded examples

--- spec/script2md_spec.rb
+++ spec/script2md_spec.rb
@@ -1,18 +1,10 @@
 RSpec.describe Script2md do
   describe '.convert' do
     subject { described_class.convert(text)  } 
-    let(:text) do
-      <<~SCRIPT
-      SCRIPT
-    end
 
     let(:text) { File.read('spec/fixtures/example.sh') }
 
     let(:converted_markdown) { File.read('spec/fixtures/example.md') }
-
-    it { is_expected.to     include '# Header1' }
-    it { is_expected.not_to include '/bin/bash' }
-    it { is_expected.not_to include '\`\`\`bash' }
 
     it { is_expected.to eq converted_markdown.chomp }
   end
