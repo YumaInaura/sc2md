c164e5e342fba2e232abaecbb41f882dc99f1ea2
Yumainaura <yuma.inaura@gmail.com>
2018-08-12 11:53:40 +0900

    Fix : script2md command does not know language type

--- spec/exe/script2md_spec.rb
+++ spec/exe/script2md_spec.rb
@@ -1,18 +1,34 @@
 RSpec.describe 'script2md' do
     subject { `script2md #{file_path}`.chomp } 
 
-    context 'when sh file' do
-      let(:file_path) { './spec/fixtures/example.sh' }
-      let(:converted_markdown) { File.read('spec/fixtures/example.sh.md') }
-
-      it { is_expected.to eq converted_markdown.chomp }
+    context 'when read from local file' do
+      context 'when sh file' do
+        let(:file_path) { './spec/fixtures/example.sh' }
+        let(:converted_markdown) { File.read('spec/fixtures/example.sh.md') }
+  
+        it { is_expected.to eq converted_markdown.chomp }
+      end
+  
+      context 'when rb file' do
+        let(:file_path) { './spec/fixtures/example.rb' }
+        let(:converted_markdown) { File.read('spec/fixtures/example.rb.md') }
+  
+        it { is_expected.to eq converted_markdown.chomp }
     end
 
-    context 'when rb file' do
-      let(:file_path) { './spec/fixtures/example.rb' }
-      let(:converted_markdown) { File.read('spec/fixtures/example.rb.md') }
-
-      it { is_expected.to eq converted_markdown.chomp }
+    context 'when read from remote file' do
+      context 'when sh file' do
+        let(:file_path) { 'https://raw.githubusercontent.com/YumaInaura/script2md/master/spec/fixtures/example.sh' }
+        let(:converted_markdown) { File.read('spec/fixtures/example.sh.md') }
+  
+        it { is_expected.to eq converted_markdown.chomp }
+      end
+  
+      context 'when rb file' do
+        let(:file_path) { 'https://raw.githubusercontent.com/YumaInaura/script2md/master/spec/fixtures/example.rb' }
+        let(:converted_markdown) { File.read('spec/fixtures/example.rb.md') }
+      end
+    end  
   end
 end
 
