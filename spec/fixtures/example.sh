b95e1051ef5637c3f7af915e9b344374bfcfee1f
Yumainaura <yuma.inaura@gmail.com>
2018-08-12 14:03:14 +0900

    Fix : Add Test : separated multiple line code block

--- spec/fixtures/example.sh
+++ spec/fixtures/example.sh
@@ -14,5 +14,8 @@ echo ok # line end code comment
 # This is comment line
 
 echo some string
-echo "i wanna conver to markdown this file"
+
+echo "i wanna conver to markdown this file" # For save my console world
+echo "i wanna make you love me"
+
 
