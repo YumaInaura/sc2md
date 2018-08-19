f3ee58bd807784298ad12e20b9071bdd97270eb5
Yumainaura <yuma.inaura@gmail.com>
2018-08-19 16:07:51 +0900

    Remove remote spec test

--- spec/exe/script2md_spec.rb
+++ spec/exe/script2md_spec.rb
@@ -17,26 +17,6 @@ RSpec.describe 'script2md' do
   
         it { is_expected.to eq converted_markdown.chomp }
     end
-
-    context 'when read from remote file' do
-      context 'when sh file' do
-        let(:file_path) { 'https://raw.githubusercontent.com/YumaInaura/script2md/master/spec/fixtures/example.sh' }
-        let(:converted_markdown) { File.read('spec/fixtures/example.sh.md').chomp }
-  
-        it 'includes markdown fragment' do
-          is_expected.to include '```'
-        end
-      end
-  
-      context 'when rb file' do
-        let(:file_path) { 'https://raw.githubusercontent.com/YumaInaura/script2md/master/spec/fixtures/example.rb' }
-        let(:converted_markdown) { File.read('spec/fixtures/example.rb.md').chomp }
-
-        it 'includes markdown fragment' do
-          is_expected.to include '```'
-        end
-      end
-    end  
   end
 end
 
