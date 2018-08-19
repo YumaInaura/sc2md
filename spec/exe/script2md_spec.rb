d89ff8198c9ae382802228edb98ecb5ca0981118
Yumainaura <yuma.inaura@gmail.com>
2018-08-19 18:20:33 +0900

    Refactor spec

--- spec/exe/script2md_spec.rb
+++ spec/exe/script2md_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe 'script2md' do
-  subject { `sc2md #{file_path}`.chomp } 
+  subject { `sc2md #{file_path}`.gsub(/[\r\n]+\z/, '') } 
 
   let(:file_path) { './spec/fixtures/output.sh' }
-  let(:converted_markdown) { File.read('./spec/fixtures/output.sh.md').chomp }
+  let(:converted_markdown) { File.read('./spec/fixtures/output.sh.md') }
   
-  it { is_expected.to eq converted_markdown.chomp }
+  it { is_expected.to eq converted_markdown.gsub(/[\r\n]+\z/, '') }
 end
 
